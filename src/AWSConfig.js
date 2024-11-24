77b86d1eac6793206db098f4c72e0dcf417b457b
Abhishek Kumar <74841098+abhishekkr1025@users.noreply.github.com>
2024-11-24 21:51:39 +0530

    Update AWSConfig.js

--- src/AWSConfig.js
+++ src/AWSConfig.js
@@ -4,7 +4,7 @@ import AWS from "aws-sdk";
 AWS.config.update({
   accessKeyId: "aws-key", // Replace with your IAM Access Key ID
   secretAccessKey: "secret-key", // Replace with your IAM Secret Access Key
-  region: "us-east-1" // Replace with your S3 bucket's region
+  region: "region" // Replace with your S3 bucket's region
 });
 
 const s3 = new AWS.S3();
